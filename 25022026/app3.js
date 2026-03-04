9dc6eda998233cb65511074edac7ae231d55c54e
CristianJZ14 <120615996+CristianJZ14@users.noreply.github.com>
2026-03-04 10:31:54 -0600

    Update app3.js

--- 25022026/app3.js
+++ 25022026/app3.js
@@ -7,3 +7,12 @@ var numero2 = parseInt(prompt("Ingresa numero 2"));
 
 var suma = numero1+numero2;
 console.log("La suma de: "+numero1+"+"+numero2+"="+suma);
+
+var resta = numero1-numero2;
+console.log("La resta de: "+numero1+"-"+numero2+"="+resta);
+
+var multiplicacion = numero1*numero2;
+console.log("La multiplicacion de: "+numero1+"*"+numero2+"="+multiplicacion);
+
+var division = numero1/numero2;
+console.log("La division de: "+numero1+"/"+numero2+"="+division);
