b7516421b5c9ea7f5cca0b03debb69981e7e0c24
CristianJZ14 <cristian_jaime_zapata@outlook.com>
2026-03-08 19:29:27 -0600

    segundatarea

---
+++ 04032026/app2.js
@@ -0,0 +1,10 @@
+let nombre = "Cristian";
+console.log("Hola \n como \n estas \n"+ nombre)
+console.log(`
+    Hola
+    como
+    estas
+    ${nombre}
+    `);
+
+    console.log(`Tu nombre es ${nombre.toUpperCase()}`);
---
+++ 04032026/app4.js
@@ -0,0 +1,19 @@
+var estado = false;
+
+if(estado){
+    var estado = false;
+}
+console.log(estado);
+
+let Estado = false;
+
+if(Estado){
+    let Estado = true;
+}
+console.log(Estado);
+
+
+for (let i=0; i<10;i++);{
+    console.log(i);
+}
+console.log(i);
